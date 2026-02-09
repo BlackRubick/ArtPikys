031862662f4ea6aa8aba6478e90f25b5dad74d1d
blackrubick <blackrubick14@gmail.com>
2026-02-09 14:48:36 -0600

    new cosas

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,19 +1,16 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
   apiKey: "AIzaSyB-JXNaSCAu4IfAX4O0U2rbVZGAEm20ZP4",
   authDomain: "pikis-7abfa.firebaseapp.com",
   projectId: "pikis-7abfa",
-  storageBucket: "pikis-7abfa.firebasestorage.app",
+  storageBucket: "pikis-7abfa.appspot.com", // Corrige: .app → .appspot.com
   messagingSenderId: "565539620077",
   appId: "1:565539620077:web:1bbe2323a3eb37610db8e4",
   measurementId: "G-41Q6PYH0M0"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
 
